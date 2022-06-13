90d4f921226dfcf1f6eb2da6e2ac93628e8ecb4b
matias032 <94572567+matias032@users.noreply.github.com>
2022-06-13 16:18:41 -0300

    routin

--- ProInt/BackEnd/src/main/java/com/portfolio/mme/Controller/PersonaController.java
+++ ProInt/BackEnd/src/main/java/com/portfolio/mme/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfolio.mme.Entity.Persona;
 import com.portfolio.mme.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired 
     private IPersonaService ipersonaService;
@@ -52,5 +54,10 @@ public class PersonaController {
      ipersonaService.savePersona(persona);
      return persona;
    }
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona() {
+        return ipersonaService.findPersona((long)1);
+    }
+    
                                 
 }
